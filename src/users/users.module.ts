7fdbe52f32d2f3b9e50a92816b83d8f3850f8e83
Christian <alvescastro00cca13@gmail.com>
2020-11-11 12:59:03 -0300

    Injected on UserService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
+import { UsersService } from './users.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserRepository])],
+    providers: [UsersService],
 })
 export class UsersModule {}
