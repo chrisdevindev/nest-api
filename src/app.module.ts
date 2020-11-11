b82cec0f3f65f7b90f83e7733fbffe307d39792f
Christian <alvescastro00cca13@gmail.com>
2020-11-11 11:43:08 -0300

    Importing the BD settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { typeOrmConfig } from './configs/typeorm.config'
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [],
   providers: [],
 })
