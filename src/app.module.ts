fd4075609bbee7369bef8aa0be33e4c7aeec3b62
Christian <alvescastro00cca13@gmail.com>
2020-11-11 12:40:11 -0300

    Entity and User table created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { typeOrmConfig } from './configs/typeorm.config'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
   controllers: [],
   providers: [],
 })
