f9b10ba0dadfb9295c222df4650a0216f7a9c5de
Christian <alvescastro00cca13@gmail.com>
2020-11-12 11:18:53 -0300

    Route of create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto'
+import { UsersService } from './users.service'
+import { ReturnUserDto } from './dtos/return-user.dto'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor (private usersService: UsersService){}
+
+    @Post()
+    async createAdminUser(
+        @Body() CreateUserDto: CreateUserDto
+    ): Promise<ReturnUserDto>{
+        const user = await this.usersService.createAdminUser(CreateUserDto)
+        return{
+            user,
+            message: 'Administrador cadastrado com sucesso'
+        }
+    }
+}
+
