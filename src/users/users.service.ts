4356ad9da547eef74bfc8359cb94d98847a8565c
Christian <alvescastro00cca13@gmail.com>
2020-11-12 10:56:07 -0300

    userService - Create User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
-
+import { CreateUserDto } from './dtos/create-user.dto'
+import { User } from './user.entity'
+import { UserRole } from './user-roles.enum'
+import { create } from 'domain';
+ 
 @Injectable()
 export class UsersService {
     constructor(
         @InjectRepository(UserRepository)
         private UserRepository: UserRepository,
     ){}
+
+    async createAdminUser(createUserDto: CreateUserDto): Promise<User>{
+        if(createUserDto.password != createUserDto.passwordConfirmation){
+            throw new UnprocessableEntityException('As senhas não conferem')
+        }else{
+            return this.UserRepository.createUser(createUserDto, UserRole.ADMIN)
+        }
+    }
 }
