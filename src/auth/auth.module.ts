4c8f8ab31a997f907ed4ca2daf3963e1c1e4e4f4
Christian <alvescastro00cca13@gmail.com>
2020-11-17 12:52:10 -0300

    Login with Token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/users/users.repository';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions:{
+        expiresIn: 18000,
+      }
+    })
+],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,33 +1,46 @@
-import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
+import { 
+    Injectable, 
+    UnauthorizedException, 
+    UnprocessableEntityException 
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { UserRole } from 'src/users/user-roles.enum';
 import { User } from 'src/users/user.entity';
 import { UserRepository } from 'src/users/users.repository';
 import { CredentialsDto } from './dtos/credentials.dto'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
-    ){}
+        private userRepository: UserRepository,
+        private jwtService: JwtService
+    ) { }
 
-    async signUp(createUserDto: CreateUserDto): Promise<User>{
-        if(createUserDto.password != createUserDto.passwordConfirmation){
+    async signUp(createUserDto: CreateUserDto): Promise<User> {
+        if (createUserDto.password != createUserDto.passwordConfirmation) {
             throw new UnprocessableEntityException('As senhas não conferem!')
-        }else{
+        } else {
             return await this.userRepository.createUser(createUserDto, UserRole.USER)
         }
     }
 
-    async signIn(credentialsDto: CredentialsDto){
+    async signIn(credentialsDto: CredentialsDto) {
         const user = await this.userRepository.checkCredentials(credentialsDto)
 
-        if( user === null){ 
+        if (user === null) {
             throw new UnauthorizedException('Credenciais inválidas')
         }
-    }
 
+        const jwtPayload = {
+            id: user.id
+        }
+
+        const token = await this.jwtService.sign(jwtPayload)
+
+        return { token }
+    }
 
 }
