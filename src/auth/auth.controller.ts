8854663a64f7049dce5982f11f1d4d66098f1172
Christian <alvescastro00cca13@gmail.com>
2020-11-17 13:07:01 -0300

    login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { AuthService } from './auth.service';
+import { CredentialsDto } from './dtos/credentials.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +15,12 @@ export class AuthController {
             message: 'Cadastro realizado com sucesso!'
         }
     }
+
+    @Post('/signin')
+    async signIn(
+        @Body(ValidationPipe) credentialsDto: CredentialsDto): Promise<{ token: string }> {
+        return await this.authService.signIn(credentialsDto)
+    }
+
 }
 
